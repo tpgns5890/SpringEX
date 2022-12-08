f43a5f2fb178138b5d737016abc0ce2f5a8dd33b
sehun <tpgns5890@naver.com>
2022-12-08 21:56:33 +0900

    1208 homework done-!

--- ex03/src/main/java/com/micol/web/board/BoardMapper.java
+++ ex03/src/main/java/com/micol/web/board/BoardMapper.java
@@ -4,4 +4,9 @@ import java.util.List;
 
 public interface BoardMapper {
 	List<BoardVO> getBoardAll(BoardVO vo);
+	int count(BoardVO vo);
+	BoardVO getBoard(int bno);
+	int insertBoard(BoardVO vo);
+	int updateBoard(BoardVO vo);
+	int deleteBoard(BoardVO vo);
 }
--- ex03/src/test/java/com/micol/web/BoardMapperClient.java
+++ ex03/src/test/java/com/micol/web/BoardMapperClient.java
@@ -18,12 +18,57 @@ public class BoardMapperClient {
 	@Autowired SqlSession mybatis;
 	@Autowired BoardMapper mapper;
 	
-	@Test
+	//@Test
 	public void getBoardAll() {
 		BoardVO vo = new BoardVO();
+		//vo.setTitle("1");
+		//vo.setWriter("user01");
 		List<BoardVO> list = mapper.getBoardAll(vo);
 		if(list != null)
 			list.forEach(board -> System.out.println(board.getContent()));
 	}
 	
+	//@Test
+	public void count() {
+		BoardVO vo = new BoardVO();
+		int r = mapper.count(vo);
+		if(r != 0)
+			System.out.println(r);
+	}
+	
+	//@Test
+	public void getBoard() {
+		BoardVO vo = mapper.getBoard(1);
+		System.out.println(vo);
+	}
+	
+	//@Test
+	public void insertBoard() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("테스트7");
+		vo.setContent("테스트내용7");
+		vo.setWriter("user07");
+		
+		int result = mapper.insertBoard(vo);
+		System.out.println(result + "건 입력됨");
+	}
+	
+	//@Test
+	public void updateBoard() {
+		BoardVO vo = new BoardVO();
+		vo.setBno(7);
+		vo.setTitle("테스트 제목7");
+		vo.setContent("테스트내용07");
+		
+		int result = mapper.updateBoard(vo);
+		System.out.println(result + "건 수정됨");
+	}
+	
+	@Test
+	public void deleteBoard() {
+		BoardVO vo = new BoardVO();
+		vo.setBno(7);
+		int result = mapper.deleteBoard(vo);
+		System.out.println(result + "건 삭제됨");
+	}
 }
